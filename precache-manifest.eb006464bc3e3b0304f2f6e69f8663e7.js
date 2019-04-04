098c55e615fcfdbe5b3e1be41fc08ba168bdae6f
Sandrockcstm <sandrockcstm@yahoo.com>
2019-04-03 22:48:00 -0500

    Updates

---
+++ precache-manifest.eb006464bc3e3b0304f2f6e69f8663e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  },
+  {
+    "revision": "bad7f747385271622353",
+    "url": "/drum-machine/static/js/main.9ae7cc19.chunk.js"
+  },
+  {
+    "revision": "66055573cce628669a5b",
+    "url": "/drum-machine/static/js/2.1f95d417.chunk.js"
+  },
+  {
+    "revision": "bad7f747385271622353",
+    "url": "/drum-machine/static/css/main.3feb9c92.chunk.css"
+  },
+  {
+    "revision": "66055573cce628669a5b",
+    "url": "/drum-machine/static/css/2.1a048021.chunk.css"
+  },
+  {
+    "revision": "67bd1036e9f404c1a49b2fb2a75d6ce4",
+    "url": "/drum-machine/index.html"
+  }
+];
